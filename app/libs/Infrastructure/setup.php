1b7f0071bbf0874e1a12ba8ee79b92abad61783a
MortalFlesh <mortal_flesh@seznam.cz>
2014-08-24 00:28:13 +0200

    - migrations added

--- app/libs/Infrastructure/setup.php
+++ app/libs/Infrastructure/setup.php
@@ -7,9 +7,19 @@ class Setup {
 	/** @var Config */
 	private $Config;
 
-	public function __construct(Database $Db, Config $Config) {
+	/** @var MigrationRepository */
+	private $MigrationLoader;
+
+	/**
+	 * @param Database $Db
+	 * @param Config $Config
+	 * @param MigrationRepository $MigrationLoader
+	 */
+	public function __construct(Database $Db, Config $Config, MigrationRepository $MigrationLoader) {
 		$this->Db = $Db;
 		$this->Config = $Config;
+
+		$this->MigrationLoader = $MigrationLoader;
 	}
 
 	public function install() {
@@ -34,5 +44,33 @@ class Setup {
 			PRIMARY KEY (`id`),
 			UNIQUE KEY `login` (`login`)
 		  ) ENGINE=MyISAM  DEFAULT CHARSET=utf8");
+
+		$this->Db->query("CREATE TABLE IF NOT EXISTS `" . $this->Config->getPrefix() . "installed_migrations` (
+			`id` int(11) NOT NULL COMMENT 'id of migration - timestamp',
+			`time_installed` datetime NOT NULL COMMENT 'time of the migration install',
+			PRIMARY KEY (`id`)
+		  ) ENGINE=MyISAM DEFAULT CHARSET=utf8");
+
+		$this->installMigrations();
+	}
+
+	private function installMigrations() {
+		$migrationsToInstall = $this->MigrationLoader->loadMigrationsToInstall();
+		
+		foreach($migrationsToInstall as $Migration) {
+			$this->installMigration($Migration);
+		}
+	}
+
+	private function installMigration(Migration $Migration) {
+		$Migration->up();
+		
+		$this->Db->query("INSERT INTO `" . $this->Config->getPrefix() . 'installed_migrations`
+			(`id`, `time_installed`) VALUES
+			(' . $Migration->getId() . ', "' . (new DateTime())->format(Database::TIME_FORMAT) . '")');
+	}
+
+	private function uninstallMigration(Migration $Migration) {
+		$Migration->down();
 	}
 }
